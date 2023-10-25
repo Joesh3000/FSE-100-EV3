9c4fec92d25d87c91b668b11b96c2dbbb3837fa1
fbright1 <149020560+fbright1@users.noreply.github.com>
2023-10-25 16:18:20 -0700

    Add files via upload

---
+++ turnAround2.m
@@ -0,0 +1,44 @@
+% brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5')
+fprintf('Running turnAround2\n');
+    angle = 335;
+    fprintf('Spinning\n');
+    brick.ResetMotorAngle('A');
+    brick.ResetMotorAngle('B');
+    brick.MoveMotorAngleRel('A', -30, angle, 'Brake'); 
+    brick.MoveMotorAngleRel('B', 30, angle, 'Brake');
+    pause(4);
+    fprintf('Done spinning\n');
+    speed = 60;
+
+    while true
+           
+           touch = brick.TouchPressed(2);
+           touch2 = brick.TouchPressed(1);
+           brick.MoveMotor('AB', -speed);
+           if touch || touch2
+               brick.StopAllMotors('Brake');
+               fprintf('Wall hit!\n');
+               fprintf('Reversing\n');
+               brick.MoveMotor('AB', speed);
+               pause(1);
+               brick.ResetMotorAngle('A');
+               brick.ResetMotorAngle('B'); 
+               
+               
+
+               brick.StopAllMotors('Brake');
+               fprintf('Turning\n');
+               brick.ResetMotorAngle('A');
+               brick.ResetMotorAngle('B'); 
+               brick.MoveMotorAngleRel('A', -30, angle, 'Brake'); 
+               brick.MoveMotorAngleRel('B', 30, angle, 'Brake'); 
+               brick.ResetMotorAngle('A');
+               brick.ResetMotorAngle('B'); 
+               pause(5);
+               brick.StopAllMotors('Brake');
+           end
+    end
+    
+
+
+             
