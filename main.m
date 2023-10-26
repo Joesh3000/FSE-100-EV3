298061396f0acf2b4093ccd76f25354831d65d20
Joesh3000 <149020359+Joesh3000@users.noreply.github.com>
2023-10-25 22:06:24 -0700

    Add files via upload

--- main.m
+++ main.m
@@ -1,36 +1,10 @@
-% clearvars -global
-% if there is a bug
-% javaclasspath('/Applications/MATLAB_R2023b.app/toolbox/ev3')
-% javaclasspath('C:\Program Files\MATLAB\R2023b\toolbox\EV3')
-% brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5');
 
-changeDetected = ultrasonic.wallChange(brick);
+fx.turnLeft(brick);
+pause(1)
+fx.turnLeft(brick); % Rotate Ultra Sensor to the Right of bot
+fx.detectWallChange(brick);
 
-if changeDetected
-    ultrasonic.goForward(brick, 60);  
-    pause(5);
-    ultrasonic.turnRight(brick, 60, 2.5); %impl gyro turns?
-    ultrasonic.goForward(brick, 60);
-    changeDetected = ultrasonic.wallChange(brick);
-    ultrasonic.goForward(brick, 60);
-    if changeDetected  %impl gyro turns? - do this again
-        ultrasonic.turnRight(brick, 60, 2.5)
-        ultrasonic.goForward(brick, 60);
-    end
-end
-
-brick.SetColorMode(3, 2);  % Set Color Sensor connected to Port 3 to Color RGB Mode.
-
-while true
-    color = brick.ColorCode(3);
-    if color == 5
-        disp('Color is red');
-        brick.StopAllMotors('Brake');
-        break;
-    end
-end
-
-
-
-%use gyro scope corrections, to keep it straight, if it hits walls it needs
-%to have a good reaction
+% Needs Gyro turns eventually
+% Needs color sensing 
+% May needs touch sensing 
+% May or may not need Wall Following (Gyro may cover this)
