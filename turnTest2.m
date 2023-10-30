6a4b040c49e8f5326fe0ca199dde0e4ce014af5a
divyanshtare <67403928+divyanshtare@users.noreply.github.com>
2023-10-30 15:05:32 -0700

    turnTest2.m

--- turnTest2.m
+++ turnTest2.m
@@ -14,5 +14,22 @@ ultrasonic.goForward(brick, 60);
 pause(2.5);
 ultrasonic.turnLeft2(brick, 30, 504); %impl gyro turns?
 pause(2);
-ultrasonic.goForward(brick, 60);
-pause(2.5);
+
+% Yellow Color Test
+brick.SetColorMode(3, 2);
+while true
+    color = brick.ColorCode(3);
+    if color == 4
+        fprintf('Lifting lift\n');
+        brick.StopAllMotors('Brake');
+        brick.MoveMotor('C', 100);
+        pause(2);
+        brick.StopAllMotors('Brake');
+        fprintf('Done Lifting\n');
+        break;
+    else
+        ultrasonic.goForward(brick, 60);
+        pause(0.25)
+    end
+end
+disp("Veer is the GOAT! :)")
