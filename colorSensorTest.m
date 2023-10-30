37f18ed2700724ad8d35691ec08334efe0d85e67
divyanshtare <67403928+divyanshtare@users.noreply.github.com>
2023-10-30 15:13:09 -0700

    Add files via upload

---
+++ colorSensorTest.m
@@ -0,0 +1,23 @@
+% brick = Brick('ioType','wifi','wfAddr','127.0.0.1','wfPort',5555,'wfSN','0016533dbaf5')
+classdef Lift
+    methods(Static)
+        function liftUp
+fprintf('running\n');
+while true
+                brick.SetColorMode(3, 2);
+                color = brick.ColorCode(3);
+                if color == 4
+                    fprintf('Lifting lift\n');
+                    brick.StopAllMotors('Brake');
+                    brick.MoveMotor('C', 250);
+                    pause(5);
+                    brick.StopAllMotors('Brake');
+                    fprintf('Done Lifting\n');
+                end
+                if color == 3
+                    fprintf('Finish\n');
+                    brick.StopAllMotors('Brake');
+                end
+
+end
+end
--- main.m
+++ main.m
@@ -1,33 +1,60 @@
 count = 0;
+% brick.SetColorMode(3, 2);
+initialDistance = brick.UltrasonicDist(4);
 
 while true
     brick.SetColorMode(3, 2);
     color = brick.ColorCode(3);
-    fx.detectWallChange(brick)
+    disp("Got Color");
     % Check for wall change
-    if fx.detectWallChange(brick)
-        count = count + 1;
-    end
+    fxcode.detectWallChange(brick, initialDistance)
+    disp("Yes I did");
+    % if fxcode.detectWallChange(brick)
+    %     count = count + 1;
+    % end
 
     % Perform actions based on color
+    % color = brick.ColorCode(3);
+    disp(color);
+    if color == 5
+        fprintf('Stop Sign \n');
+        % brick.StopAllMotors('Brake');
+    end
     if color == 4
         fprintf('Lifting lift\n');
         brick.StopAllMotors('Brake');
-        brick.MoveMotor('C', 250);
-        pause(5);
+        brick.MoveMotor('C', 100);
+        pause(2);
         brick.StopAllMotors('Brake');
         fprintf('Done Lifting\n');
     end
-    
     if color == 3
         fprintf('Finish\n');
         brick.StopAllMotors('Brake');
     end
-    
-    if color == 5
-        brick.StopAllMotors('Brake');
-        fprintf('yo')
-    end
+
+
+
+    %  Lift.liftUp(brick);
+
+    % if color == 4
+    %     fprintf('Lifting lift\n');
+    %     brick.StopAllMotors('Brake');
+    %     brick.MoveMotor('C', 250);
+    %     pause(5);
+    %     brick.StopAllMotors('Brake');
+    %     fprintf('Done Lifting\n');
+    % end
+    % 
+    % if color == 3
+    %     fprintf('Finish\n');
+    %     brick.StopAllMotors('Brake');
+    % end
+    % 
+    % if color == 5
+    %     brick.StopAllMotors('Brake');
+    %     fprintf('yo')
+    % end
 
     pause(1);
 end
